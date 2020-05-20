24545418289f229e0e727393a89d8628c6d52b43
caseychester7414 <casey.chester@bryanuniversity.edu>
2020-05-20 13:45:31 -0500

    axios

---
+++ week6/application/js.js
@@ -0,0 +1,6 @@
+axios.get("https://rickandmortyapi.com/api/character/61")
+axios.get("https://rickandmortyapi.com/api/character/332")
+axios.get("https://rickandmortyapi.com/api/character/201")
+.then(response=>{
+
+})
