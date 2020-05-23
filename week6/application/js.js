9f2ab802b562b248cee045bba3c809764c1e587b
caseychester7414 <casey.chester@bryanuniversity.edu>
2020-05-23 11:41:01 -0500

    postman

--- week6/application/js.js
+++ week6/application/js.js
@@ -1,6 +1,33 @@
 axios.get("https://rickandmortyapi.com/api/character/61")
-axios.get("https://rickandmortyapi.com/api/character/332")
+
+
+.then(response=>{
+    //    for(let i = 0; i < response.data.length; i++){
+    //     const data= response.data[i]
+    //     showData(data.object[0].character/61)
+    //      console.log(data.object[0].character/61)
+    console.log(response.data)
+})
+axios.get("https://rickandmortyapi.com/api/character/332")     
+.then(response=>{
+    //  for(let i = 0; i < response.data.length; i++){
+        
+         
+        
+    console.log(response.data)
+})
 axios.get("https://rickandmortyapi.com/api/character/201")
 .then(response=>{
-
+    //  for(let i = 0; i < response.data.length; i++){
+        
+    console.log(response.data)
 })
+//         function showData(arr){
+//             for(let i = 0; i < arr.length; i++){
+//                 const h1 = document.createElement('h1')
+//                 h1.textContent = arr[i].name
+//                 document.body.appendChild(h1)
+//             }
+// }}})
+
+   
