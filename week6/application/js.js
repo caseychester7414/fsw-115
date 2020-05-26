b514a1acc7ded2c4801ac72e4f3ee441ef92b30e
caseychester7414 <casey.chester@bryanuniversity.edu>
2020-05-25 21:30:46 -0500

    capstone

--- week6/application/js.js
+++ week6/application/js.js
@@ -1,6 +1,52 @@
 axios.get("https://rickandmortyapi.com/api/character/61")
 .then(response=>{
-    // var rickCreated = document.createElement("h1")
+ 
+
+       for(let i = 0; i < Object.values(response.data).length; i++){
+        var rick = document.createElement("h3")
+        document.body.appendChild(rick)
+         rick.textContent = Object.values(response.data)[i]
+         if(typeof Object.values(response.data)[i] =="object" ){
+rick.textContent = Object.values(response.data)[i].name
+         }
+       }
+       
+
+    console.log(Object.values(response.data))
+})
+axios.get("https://rickandmortyapi.com/api/character/332")     
+.then(response=>{
+    for(let i = 0; i < Object.values(response.data).length; i++){
+        var rick = document.createElement("h4")
+        document.body.appendChild(rick)
+         rick.textContent = Object.values(response.data)[i]
+         if(typeof Object.values(response.data)[i] =="object" ){
+            rick.textContent = Object.values(response.data)[i].name
+                     }
+       }
+        
+         
+        
+    console.log(response.data)
+})
+axios.get("https://rickandmortyapi.com/api/character/201")
+.then(response=>{
+    for(let i = 0; i < Object.values(response.data).length; i++){
+        var rick = document.createElement("h5")
+        document.body.appendChild(rick)
+         rick.textContent = Object.values(response.data)[i]
+         if(typeof Object.values(response.data)[i] =="object" ){
+            rick.textContent = Object.values(response.data)[i].name
+                     }
+       }
+        
+    console.log(response.data)
+})
+
+
+
+
+   // var rickCreated = document.createElement("h1")
     // document.body.appendChild(rickCreated)
     // rickCreated.textContent = Object.values(response.data)[0]
 
@@ -47,47 +93,5 @@ axios.get("https://rickandmortyapi.com/api/character/61")
     //     var rickUrl = document.createElement("h4")
     // document.body.appendChild(rickUrl)
     // rickUrl.textContent = Object.values(response.data)[11]
-
-       for(let i = 0; i < Object.values(response.data).length; i++){
-        var rick = document.createElement("h3")
-        document.body.appendChild(rick)
-         rick.textContent = Object.values(response.data)[i]
-         if(typeof Object.values(response.data)[i] =="object" ){
-rick.textContent = Object.values(response.data)[i].name
-         }
-       }
-       
-
-    console.log(Object.values(response.data))
-})
-axios.get("https://rickandmortyapi.com/api/character/332")     
-.then(response=>{
-    for(let i = 0; i < Object.values(response.data).length; i++){
-        var rick = document.createElement("h4")
-        document.body.appendChild(rick)
-         rick.textContent = Object.values(response.data)[i]
-         if(typeof Object.values(response.data)[i] =="object" ){
-            rick.textContent = Object.values(response.data)[i].name
-                     }
-       }
-        
-         
-        
-    console.log(response.data)
-})
-axios.get("https://rickandmortyapi.com/api/character/201")
-.then(response=>{
-    for(let i = 0; i < Object.values(response.data).length; i++){
-        var rick = document.createElement("h5")
-        document.body.appendChild(rick)
-         rick.textContent = Object.values(response.data)[i]
-         if(typeof Object.values(response.data)[i] =="object" ){
-            rick.textContent = Object.values(response.data)[i].name
-                     }
-       }
-        
-    console.log(response.data)
-})
-
 
    
---
+++ week7/capstone/js.js
@@ -0,0 +1,76 @@
+const form = document.mainForm
+
+const liElements =[]
+const myWrapper = document.getElementById("myListWithButtons")
+const listUL = document.getElementById("theList");
+let test = document.getElementById("test") // my test H1 on the DOM
+const who = document.getElementById("assign");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let inputValue = document.getElementById("assign").value
+    let dothis  = document.getElementById("dothis").value
+    if (inputValue === "" ){
+        alert("fill the open boxes!")
+    }else{
+        addListToDOM(inputValue)
+    }
+    document.getElementById("assign").value = "" ;//reset the input so they can add again\
+    document.getElementById("dothis").value=""
+})
+function addListToDOM(taskedItem){
+    let theListItem = createTaskElement(taskedItem)
+    
+    listUL.append(theListItem);
+}
+function  createTaskElement(addToLists){
+    let listItem = document.createElement("li")
+    let theLabel = document.createElement("label")
+    let deleteButton = document.createElement("button")
+    let who = document.getElementById("dothis").value
+    theLabel.innerText = who + "   : " + addToLists + "." ;
+    deleteButton.innerText = "Delete";
+    listItem.onclick = clickedTask
+    deleteButton.onclick = deleteAction
+    deleteButton.className = "delete"
+    listItem.appendChild(theLabel)
+    listItem.appendChild(deleteButton)
+    return listItem    
+}
+function deleteAction(){
+    
+    this.parentNode.parentNode.removeChild(this.parentNode)
+}
+function clickedTask(){
+   
+this.classList.toggle("checked")
+}
+
+document.getElementById("button3").addEventListener("click", ()=>{
+    axios.get("https://rickandmortyapi.com/api/character/326")
+.then(response=>{
+    console.log(response)
+    for(let i = 0; i < Object.values(response.data).length; i++){
+        var rick = document.createElement("h3")
+        document.body.appendChild(rick)
+         rick.textContent = Object.values(response.data)[i]
+         if(typeof Object.values(response.data)[i] =="object" ){
+rick.textContent = Object.values(response.data)[i].name
+         }
+       }
+})})
+
+
+document.getElementById("button4").addEventListener("click", ()=>{
+    axios.get("https://rickandmortyapi.com/api/character/52")
+.then(response=>{
+    console.log(response)
+    for(let i = 0; i < Object.values(response.data).length; i++){
+        var rick = document.createElement("h3")
+        document.body.appendChild(rick)
+         rick.textContent = Object.values(response.data)[i]
+         if(typeof Object.values(response.data)[i] =="object" ){
+rick.textContent = Object.values(response.data)[i].name
+         }
+       }
+})})
